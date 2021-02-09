82056dd931841298e6522788cb03b2be480c1348
jungwook <neity16@daum.net>
2021-02-09 18:12:43 +0900

    placeholder수정 및 입력 제한

--- sopkathon27th/src/pages/LandingPage.js
+++ sopkathon27th/src/pages/LandingPage.js
@@ -1,15 +1,20 @@
-import React from 'react';
+import React from "react";
 
-import './Landing.scss';
+import "./Landing.scss";
 
 function LandingPage({ onBirthHandler, props }) {
   const startTest = () => {
-    const UserYear = document.getElementById('year');
+    const UserYear = document.getElementById("year");
 
     try {
-      if (UserYear.value < 1900 || UserYear.value > 2020) throw '1900년 이후 출생자만 지원가능.';
+      if (
+        UserYear.value < 1900 ||
+        UserYear.value > 2020 ||
+        isNaN(UserYear.value)
+      )
+        throw "1900년 이후 출생자만 지원가능.";
 
-      props.history.push('/question/1');
+      props.history.push("/question/1");
 
       onBirthHandler(UserYear.value);
     } catch (e) {
@@ -21,11 +26,17 @@ function LandingPage({ onBirthHandler, props }) {
     <div class="root">
       <form
         class="input"
-        onSubmit={e => {
+        onSubmit={(e) => {
           e.preventDefault();
         }}
       >
-        <input class="input__text" type="text" id="year" required="required" placeholder="출생연도를 입력(예시: 1997)"></input>
+        <input
+          class="input__text"
+          type="text"
+          id="year"
+          required="required"
+          placeholder="출생연도 입력(예시: 1997)"
+        ></input>
 
         <button class="input__button" onClick={startTest}>
           테스트 시작
